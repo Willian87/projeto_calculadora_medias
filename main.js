89aa84fad4a05fe71f1baeae7d586bbf8b37e2a6
Willian Oliveira <willian-tito@hotmail.com>
2024-02-12 21:09:59 -0500

    fix: 2 casas decimais

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function atualizarMediaFinal(){
     
     const mediaFinal = calculoMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinina ? spanAprovado : spanReprovado;
     
 }
